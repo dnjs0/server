bdc9973d29bdd1bb3e109ee2fd244089ba28324a
dnjs0 <heatherray@naver.com>
2025-03-10 09:16:01 +0900

    JABC

---
+++ JDBCTest/localhost.server.sql
@@ -0,0 +1,19 @@
+-- 새 계정 생성
+
+--확인하기
+/*
+1.serveice.msc
+- OracleService XXE
+- OrecleDraDB21Home1TNSListener
+
+*/
+
+
+show user; --USER이(가) "SYSTEM"입니다.
+
+
+ alter session set "_ORACLE_SCRIPT"=true;
+create user server identified by java1234;
+grant connect, resource, dba to server;
+alter user server default tablespace users;
+-- 새접속 -> 이름: localhost.server, 비번: java1234
---
+++ JDBCTest/src/com/test/java/Ex01.java
@@ -0,0 +1,14 @@
+package com.test.java;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        /*
+          
+          JDBC, Java Database Connectivity
+          
+         
+         *
+         */
+    }
+
+}
